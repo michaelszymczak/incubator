6d30568aeef09493f8212ea97e224e67b385f8cf
Michael Szymczak <contact@michaelszymczak.com>
2021-07-03 19:03:47 +0100

    assertJ dependency

---
+++ fixdiff/src/test/java/com/michaelszymczak/sample/DiffTest.java
@@ -0,0 +1,14 @@
+package com.michaelszymczak.sample;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DiffTest
+{
+    @Test
+    void foo()
+    {
+        assertThat(true).isEqualTo(true);
+    }
+}
